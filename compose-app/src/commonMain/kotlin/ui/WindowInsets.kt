44e32f37e3aa59004cee8ccfed4b071749cd7e25
LTFan <xfqwdsj@qq.com>
2025-06-27 13:47:38 +0800

    feat: 全局定义默认应用WindowInsets

---
+++ compose-app/src/commonMain/kotlin/ui/WindowInsets.kt
@@ -0,0 +1,7 @@
+package top.ltfan.labailearn.ui
+
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.safeContent
+import androidx.compose.runtime.Composable
+
+val AppWindowInsets @Composable get() = WindowInsets.safeContent
