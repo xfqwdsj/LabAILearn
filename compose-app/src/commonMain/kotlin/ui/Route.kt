ef792def208cf5ca0114944ac03b9eee0eed7b0c
LTFan <xfqwdsj@qq.com>
2025-06-27 16:46:49 +0800

    feat: 主页切换动画

--- compose-app/src/commonMain/kotlin/ui/Route.kt
+++ compose-app/src/commonMain/kotlin/ui/Route.kt
@@ -1,15 +1,14 @@
 package top.ltfan.labailearn.ui
 
-import androidx.compose.animation.ExperimentalSharedTransitionApi
-import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.*
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Hardware
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.SpaceDashboard
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.navigation.NavController
-import androidx.navigation.NavGraphBuilder
+import androidx.navigation.*
 import androidx.navigation.compose.composable
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.Transient
@@ -18,9 +17,12 @@ import labailearn.compose_app.generated.resources.label_navigation_overview
 import labailearn.compose_app.generated.resources.label_navigation_settings
 import labailearn.compose_app.generated.resources.label_navigation_tools
 import org.jetbrains.compose.resources.StringResource
+import top.ltfan.labailearn.ui.component.BlurEnterExit
 import top.ltfan.labailearn.ui.pages.main.OverviewPage
 import top.ltfan.labailearn.ui.pages.main.SettingsPage
 import top.ltfan.labailearn.ui.pages.main.ToolsPage
+import kotlin.jvm.JvmSuppressWildcards
+import kotlin.reflect.KType
 
 @OptIn(ExperimentalSharedTransitionApi::class)
 @Serializable
@@ -44,7 +46,7 @@ abstract class Route {
             override val icon = Icons.Default.SpaceDashboard
 
             context(viewModel: AppViewModel, n: NavController, s: SharedTransitionScope, p: PaddingValues) override fun NavGraphBuilder.builder() {
-                composable<Overview> { viewModel.OverviewPage() }
+                composableWithSlideTransition<Overview> { BlurEnterExit { viewModel.OverviewPage() } }
             }
         }
 
@@ -57,7 +59,7 @@ abstract class Route {
             override val icon = Icons.Default.Hardware
 
             context(viewModel: AppViewModel, n: NavController, s: SharedTransitionScope, p: PaddingValues) override fun NavGraphBuilder.builder() {
-                composable<Tools> { viewModel.ToolsPage() }
+                composableWithSlideTransition<Tools> { BlurEnterExit { viewModel.ToolsPage() } }
             }
         }
 
@@ -70,8 +72,26 @@ abstract class Route {
             override val icon = Icons.Default.Settings
 
             context(viewModel: AppViewModel, n: NavController, s: SharedTransitionScope, p: PaddingValues) override fun NavGraphBuilder.builder() {
-                composable<Settings> { viewModel.SettingsPage() }
+                composableWithSlideTransition<Settings> { BlurEnterExit { viewModel.SettingsPage() } }
             }
+        }
+
+        inline fun <reified T : Any> NavGraphBuilder.composableWithSlideTransition(
+            typeMap: Map<KType, @JvmSuppressWildcards NavType<*>> = emptyMap(),
+            deepLinks: List<NavDeepLink> = emptyList(),
+            noinline sizeTransform: (AnimatedContentTransitionScope<NavBackStackEntry>.() -> @JvmSuppressWildcards SizeTransform?)? = null,
+            noinline content: @Composable (AnimatedContentScope.(NavBackStackEntry) -> Unit)
+        ) {
+            composable<T>(
+                typeMap = typeMap,
+                deepLinks = deepLinks,
+                enterTransition = { slideInHorizontally { it / 2 } + fadeIn() },
+                exitTransition = { slideOutHorizontally() + fadeOut() },
+                popEnterTransition = { slideInHorizontally() + fadeIn() },
+                popExitTransition = { slideOutHorizontally { it / 2 } + fadeOut() },
+                sizeTransform = sizeTransform,
+                content = content
+            )
         }
     }
 }
---
+++ compose-app/src/commonMain/kotlin/ui/component/BlurEnterExit.kt
@@ -0,0 +1,22 @@
+package top.ltfan.labailearn.ui.component
+
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.EnterExitState
+import androidx.compose.animation.core.animateDp
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun AnimatedVisibilityScope.BlurEnterExit(
+    modifier: Modifier = Modifier, maxRadius: Dp = 8.dp, content: @Composable BoxScope.() -> Unit
+) {
+    val blurRadius by transition.animateDp { if (it != EnterExitState.PostExit) 0.dp else maxRadius }
+
+    Box(modifier.blur(blurRadius), content = content)
+}
